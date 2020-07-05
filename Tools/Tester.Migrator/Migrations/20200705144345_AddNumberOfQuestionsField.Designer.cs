bc5c40b7c31eb28b3101bdeb0fef5d916441f559
illusionist nemo <illusionist.nemo@gmail.com>
2020-07-05 17:44:25 +0300

    add migration

--- Tools/Tester.Migrator/Migrations/20200705144345_AddNumberOfQuestionsField.Designer.cs
+++ Tools/Tester.Migrator/Migrations/20200705144345_AddNumberOfQuestionsField.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Tester.Core.Common;
@@ -10,9 +11,10 @@ using Tester.Migrator;
 namespace Tester.Migrator.Migrations
 {
     [DbContext(typeof(MigratorEfDataConnection))]
-    partial class MigratorEfDataConnectionModelSnapshot : ModelSnapshot
+    [Migration("20200705144345_AddNumberOfQuestionsField")]
+    partial class AddNumberOfQuestionsField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Tools/Tester.Migrator/Migrations/20200705144345_AddNumberOfQuestionsField.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tester.Migrator.Migrations
+{
+    public partial class AddNumberOfQuestionsField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOver",
+                schema: "report",
+                table: "user_test",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfQuestions",
+                schema: "app",
+                table: "test",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOver",
+                schema: "report",
+                table: "user_test");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfQuestions",
+                schema: "app",
+                table: "test");
+        }
+    }
+}
